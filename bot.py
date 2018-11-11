1c588ae466d580c7d0a67f693566f0417b0a3be8
Janek <j.haberer@gmx.net>
2018-11-11 04:38:58 -0800

    gelesen verbessert, willkommen protected, invite command added

--- bot.py
+++ bot.py
@@ -1,34 +1,51 @@
 import asyncio
-from discord import Game
-from discord.ext import commands
+from discord import Game, Client
 from bottoken import TOKEN
 
-BOT_PREFIX = "!"
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(BOT_PREFIX))
+client = Client()
 
-@bot.command(name='gelesen',
-                pass_context=True)
-async def on_message(context):
+
+async def gelesen(msg):
     role = None
-    for i in context.message.server.roles:
+    for i in msg.server.roles:
         if str(i) == 'Community':
             role = i
-    await bot.add_roles(context.message.author, role)
-    await bot.delete_message(context.message)
+    if role is None:
+        print('Community Role wurde nicht gefunden')
+        await client.send_message(msg.author, 'Anscheinend gibt es ein Problem beim Zuweisen der Rolle, '
+                                              'bitte wende Dich an einen Admin :)')
+    else:
+        await client.add_roles(msg.author, role)
 
-@bot.event
+
+async def invite(msg):
+    await client.send_message(msg.channel, 'https://discord.gg/duCsY9U')
+
+
+@client.event
 async def on_ready():
-    await bot.change_presence(game=Game(name="ist ZORNI"))
-    print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
+    await client.change_presence(game=Game(name="ist ZORNI"))
+    print('Logged in as:\n{0} (ID: {0.id})'.format(client.user))
+
+
+@client.event
+async def on_message(msg):
+    if msg.channel.name == 'willkommen':
+        if msg.content == '?gelesen':
+            await gelesen(msg)
+        await client.delete_message(msg)
+    elif msg.channel.name == 'bot':
+        if msg.content == '?invite':
+            await invite(msg)
 
 
 async def list_servers():
-    await bot.wait_until_ready()
-    while not bot.is_closed:
+    await client.wait_until_ready()
+    while not client.is_closed:
         print("Current servers:")
-        for server in bot.servers:
+        for server in client.servers:
             print(server.name)
         await asyncio.sleep(600)
 
-bot.loop.create_task(list_servers())
-bot.run(TOKEN)
+client.loop.create_task(list_servers())
+client.run(TOKEN)
