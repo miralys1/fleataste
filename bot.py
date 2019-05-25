350477326a4d5b0e3e7ffcf94fa082c66c3c30e4
Janek <j.haberer@gmx.net>
2019-05-25 07:00:20 -0700

    migrated to rewrite discord.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
-from discord import Game, Client
+from discord import Client
+import discord
 from bottoken import TOKEN
 
 client = Client()
@@ -7,45 +8,47 @@ client = Client()
 
 async def gelesen(msg):
     role = None
-    for i in msg.server.roles:
+    for i in msg.guild.roles:
         if str(i) == 'Community':
             role = i
     if role is None:
         print('Community Role wurde nicht gefunden')
-        await client.send_message(msg.author, 'Anscheinend gibt es ein Problem beim Zuweisen der Rolle, '
-                                              'bitte wende Dich an einen Admin :)')
+        await msg.author.send('Anscheinend gibt es ein Problem beim Zuweisen der Rolle, '
+                              'bitte wende Dich an einen Admin :)')
     else:
-        await client.add_roles(msg.author, role)
+        await msg.author.add_roles(role)
 
 
 async def invite(msg):
-    await client.send_message(msg.channel, 'https://discord.gg/duCsY9U')
+    await msg.channel.send('https://discord.gg/duCsY9U')
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name="ist ZORNI"))
+    activity = discord.Game("ist ZORNI")
+    await client.change_presence(activity=activity)
     print('Logged in as:\n{0} (ID: {0.id})'.format(client.user))
 
 
 @client.event
 async def on_message(msg):
-    if msg.channel.name == 'willkommen':
-        if msg.content == '?gelesen':
-            await gelesen(msg)
-        await client.delete_message(msg)
-    elif msg.channel.name == 'bot':
-        if msg.content == '?invite':
-            await invite(msg)
+    if isinstance(msg.channel, discord.abc.GuildChannel):
+        if msg.channel.name == 'willkommen':
+            if msg.content == '?gelesen':
+                await gelesen(msg)
+            await msg.delete()
+        elif msg.channel.name == 'bot':
+            if msg.content == '?invite':
+                await invite(msg)
 
 
-async def list_servers():
+async def list_guilds():
     await client.wait_until_ready()
-    while not client.is_closed:
-        print("Current servers:")
-        for server in client.servers:
-            print(server.name)
+    while not client.is_closed():
+        print("Current guilds:")
+        for guild in client.guilds:
+            print(guild.name)
         await asyncio.sleep(600)
 
-client.loop.create_task(list_servers())
+client.loop.create_task(list_guilds())
 client.run(TOKEN)
